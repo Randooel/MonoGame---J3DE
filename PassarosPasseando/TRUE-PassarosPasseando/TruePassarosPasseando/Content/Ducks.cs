f87a28dabb347fa5b07ab199686e874423e12bdd
Leonardo Barros <leonardo.alves.barros.1@gmail.com>
2025-05-23 17:38:38 -0300

    Wip passaros passeando

---
+++ PassarosPasseando/TRUE-PassarosPasseando/TruePassarosPasseando/Content/Ducks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruePassarosPasseando.Content
+{
+    internal class Ducks
+    {
+        #region VARIABLES
+        Texture2D sprite;
+        Vector2 position, speed;
+        Point size;
+        Color color;
+        #endregion
+
+        public Ducks(Texture2D sprite, Vector2 position, Point size, Color color)
+        {
+            this.sprite = sprite;
+            this.position = position;
+            this.size = size;
+            this.color = color;
+        }
+    }
+}
---
+++ PassarosPasseando/TRUE-PassarosPasseando/TruePassarosPasseando/Game1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using TruePassarosPasseando.Content;
+
+namespace TruePassarosPasseando
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Ducks[] ducks;
+        Random rand;
+
+        const int maxDucks = 5000;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            ducks = new Ducks[maxDucks];
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D image = Content.Load<Texture2D>("duck");      
+            
+            for (int i = 0; i < maxDucks; i++)
+            {
+                int w =  rand.Next(16, 219);
+                int h = rand.Next(16, 219);
+                ducks[i] = new Ducks(image, position, size, Color.)
+            }
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
