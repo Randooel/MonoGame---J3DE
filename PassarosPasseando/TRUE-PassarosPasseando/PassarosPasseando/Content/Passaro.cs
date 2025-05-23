88c98a9048313ce82d5da9fdffd30a626d6f8b9b
Leonardo Barros <leonardo.alves.barros.1@gmail.com>
2025-05-23 17:11:10 -0300

    PROJETO VERDADEIRO

---
+++ PassarosPasseando/TRUE-PassarosPasseando/PassarosPasseando/Content/Passaro.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassarosPasseando.Content
+{
+    class Passaro
+    {
+        #region VARIABLES
+        Texture2D sprite;
+        Vector2 position;
+        float speed;
+        #endregion
+
+        public void Passar()
+        {
+
+        }
+    }
+}
---
+++ PassarosPasseando/TRUE-PassarosPasseando/PassarosPasseando/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new PassarosPasseando.Game1();
+game.Run();
